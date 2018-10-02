0431da4ab9473d9a0829aabd3b282ef4884895fb
Benjamin Purtzer <reptilebuyer@hotmail.com>
2018-10-01 20:45:14 -0700

    idk

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//can use .post or other requests
+//do not have to use /routes/api/test because we have already included that in server.js
+router.get('/test', (req, res) => res.json({msg: "Users Works"}));
+
+//need to export for the server.js file to pick up
+module.exports = router;
+
