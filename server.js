326015f104ac59e98a400f4bfa4afce00c430bde
Benjamin Purtzer <reptilebuyer@hotmail.com>
2018-10-01 19:48:17 -0700

    setup server.js file for beginning

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const path = require('path');
+const express = require("express");
+const util = require('util');
+const fs = require('fs');
+const mongo = require('mongodb').MongoClient;
+const http = require('http');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('hello')); 
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
